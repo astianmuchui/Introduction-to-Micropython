72a03efd7234001d559cb98e21cb00140a2b1647
Sebastian M <Sebastianmuchui79@gmail.com>
2022-06-25 09:23:25 +0300

    Update blink.py

--- blink.py
+++ blink.py
@@ -8,4 +8,4 @@ def tick(timer):
     global led, LED_state
     LED_state = not LED_state
     led.value(LED_state)
-tim.init(freq=1,mode=Timer.PERIODIC,callback=tick)
+tim.init(freq=11,mode=Timer.PERIODIC,callback=tick)
